01ff11675574e821a25ee3d42910198a7aef0b61
zubair-zm2108715 <157352075+zubair-zm2108715@users.noreply.github.com>
2024-10-31 15:17:58 +0300

    Update System.sh with free memory percentage

--- Shell Scripts/System.sh
+++ Shell Scripts/System.sh
@@ -19,6 +19,7 @@ echo -e "\n=== Memory and CPU Information ===" | tee -a $MEM_CPU_LOG
 free_memory=$(free | grep Mem | awk '{print $4}')
 total_memory=$(free | grep Mem | awk '{print $2}')
 used_memory_percentage=$(( (total_memory - free_memory) * 100 / total_memory ))
+free_memory_percentage=$(( free_memory * 100 / total_memory ))
 
 # Get CPU model and number of cores
 cpu_model=$(grep -m 1 'model name' /proc/cpuinfo | awk -F: '{print $2}' | xargs)
@@ -26,6 +27,7 @@ cpu_cores=$(nproc)
 
 # Display memory usage
 echo "Used Memory Percentage: $used_memory_percentage%" | tee -a $MEM_CPU_LOG
+echo "Free Memory Percentage: $free_memory_percentage%" | tee -a $MEM_CPU_LOG
 echo "CPU Model: $cpu_model" | tee -a $MEM_CPU_LOG
 echo "Number of CPU Cores: $cpu_cores" | tee -a $MEM_CPU_LOG
 
